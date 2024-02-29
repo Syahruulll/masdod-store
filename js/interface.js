8e50a24e7647cc4e0c3a8886e8e613808ac70f53
syahruulll <@01syahrulhidayattullah@gmail.com>
2024-02-29 21:02:23 +0700

    first

---
+++ js/interface.js
@@ -0,0 +1,16 @@
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+function openCity(cityName) {
+  var i;
+  var x = document.getElementsByClassName("city");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  document.getElementById(cityName).style.display = "block";  
+}
